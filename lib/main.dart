e0811a1a3aa14a208c1ad97a3314fcf3b0cc6578
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-11-15 21:37:12 +0200

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:doc_doc/core/di/dependency_injection.dart';
 import 'package:doc_doc/doc_app.dart';
 import 'package:flutter/material.dart';
-
 import 'core/routing/app_router.dart';
 
 void main() {
+  setupGetIt();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
