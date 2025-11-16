402462fc9bed9b8e453ee8b4f9b94a1b96880b93
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-11-16 16:26:41 +0200

    Added Android Flavors ✅

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,14 @@
 import 'package:doc_doc/core/di/dependency_injection.dart';
 import 'package:doc_doc/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'core/routing/app_router.dart';
 
-void main() {
+void main() async{
   setupGetIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
